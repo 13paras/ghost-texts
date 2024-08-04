999b0b104d6076da12117373f265ba5a8e78976f
Paras <sakarwalparas@gmail.com>
2024-08-04 16:53:15 +0530

    removed console form middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,11 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ 
+  const token = await getToken({
     req: request,
-    secret: process.env.NEXTAUTH_SECRET_KEY 
+    secret: process.env.NEXTAUTH_SECRET_KEY,
   });
-  
-  console.log("Token grabbed from middleware", { token });
+
   const url = request.nextUrl;
 
   if (
