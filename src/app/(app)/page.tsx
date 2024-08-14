83670a690ee38e9e5db817d3d60046c8e40d821c
Paras <sakarwalparas@gmail.com>
2024-08-14 20:30:40 +0530

    updated home page design

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -15,47 +15,53 @@ import {
 import Autoplay from "embla-carousel-autoplay";
 import { MessageCircle } from "lucide-react";
 import messages from "../../messages.json";
+import { Spotlight } from "../_components/ui/Spotlight";
 
 export default function Home() {
   return (
     <main>
-      {/* Meteor BG */}
-      <div className="relative flex h-[500px] w-full flex-col items-center justify-center space-y-5 overflow-hidden rounded-lg bg-background md:shadow-xl">
-        <Meteors number={30} />
-        <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-6xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-        Dive into the world of Ghostly Whispers
-        </span>
-        <p className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-lg font-semibold leading-none text-transparent dark:from-white dark:to-slate-800/10">
-        Unleash your inner mischief with Ghost Text - where your secrets stay safe
-        </p>
-      </div>
-      <div className="flex flex-col items-center">
-        {/* Carousel for Messages */}
-        <Carousel
-          plugins={[Autoplay({ delay: 2000 })]}
-          className="w-full max-w-lg md:max-w-xl"
-        >
-          <CarouselContent>
-            {messages.map((message, index) => (
-              <CarouselItem key={index} className="p-4">
-                <Card className="border-gray-50/[.1] bg-gray-50/[.10] transition hover:bg-gray-50/[.15]">
-                  <CardHeader>
-                    <CardTitle>{message.title}</CardTitle>
-                  </CardHeader>
-                  <CardContent className="flex flex-col items-start space-y-2 md:flex-row md:space-x-4 md:space-y-0">
-                    <MessageCircle className="flex-shrink-0" />
-                    <div>
-                      <p>{message.content}</p>
-                      <p className="text-xs text-muted-foreground">
-                        {message.received}
-                      </p>
-                    </div>
-                  </CardContent>
-                </Card>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-        </Carousel>
+     
+      <div className="bg-grid-white/[0.02] relative flex h-[40rem] w-full flex-col overflow-hidden rounded-md bg-black/[0.96] antialiased md:items-center md:justify-center">
+        <Spotlight
+          className="-top-40 left-0 md:-top-20 md:left-60"
+          fill="white"
+        />
+        <div className="relative z-10 mx-auto w-full max-w-7xl p-4 pt-20 md:pt-0">
+          <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-7xl">
+          Dive into the world of  <br /> Ghostly Whispers.
+          </h1>
+          <p className="mx-auto mt-4 max-w-lg text-center text-base font-normal text-neutral-300">
+          Unleash your inner mischief with Ghost Text - where your secrets stay safe
+          </p>
+        </div>
+        <div className="mt-10 flex mx-10 md:mx-0 flex-col items-center">
+          {/* Carousel for Messages */}
+          <Carousel
+            plugins={[Autoplay({ delay: 2000 })]}
+            className="w-full max-w-sm md:max-w-xl"
+          >
+            <CarouselContent>
+              {messages.map((message, index) => (
+                <CarouselItem key={index} className="p-4">
+                  <Card className="border-gray-50/[.1] bg-gray-50/[.10] transition hover:bg-gray-50/[.15]">
+                    <CardHeader>
+                      <CardTitle className="text-xl md:text-2xl">{message.title}</CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex gap-2 items-center md:items-start space-y-2 flex-row md:space-x-4 md:space-y-0">
+                      <MessageCircle className="flex-shrink-0" />
+                      <div>
+                        <p>{message.content}</p>
+                        <p className="text-xs text-muted-foreground">
+                          {message.received}
+                        </p>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+        </div>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,6 +59,16 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -80,6 +90,7 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         meteor: "meteor 5s linear infinite",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
     },
   },
